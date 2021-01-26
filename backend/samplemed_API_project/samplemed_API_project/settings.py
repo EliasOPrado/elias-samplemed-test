c97e8b4a05a9d4f8905441ff03debfe8306a8128
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-01-26 07:16:14 -0300

    added connection

--- backend/samplemed_API_project/samplemed_API_project/settings.py
+++ backend/samplemed_API_project/samplemed_API_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # samplemed test - apps
+    'corsheaders',
     'rest_framework.authtoken',
     'rest_framework',
     'sm_api_app',
@@ -36,11 +37,20 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Para conexao com react
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+  
 ]
 
 ROOT_URLCONF = 'samplemed_API_project.urls'
--- frontend/samplemed_frontend_project/src/App.js
+++ frontend/samplemed_frontend_project/src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() =>{
+    fetch('http://127.0.0.1:8000/articles/', {
+      'method': 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Token 86a65a3b92e8f791fde3eac28f8cc6c130e44e1d'
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setArticles(resp)
+    ).catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      
+       <h1>Samplemed Test - Blog</h1>
+      
+      {articles.map(article => {
+        return (
+        <div key={article.id}>
+          <h2>{article.title}</h2>
+          <p>{article.description}</p>
+        </div>
+        )
+      })}
     </div>
   );
 }
